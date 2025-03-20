67e6345867e111122411cad526456c33c5018ef0
linky <potapov.roma@mail.ru>
2025-03-20 16:03:13 +0300

    first commit2

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <Head>
+    <Title>Lumatica - интернет магазин освещения</Title>
+    <Meta
+      name="description"
+      content="Магазин освещения в Краснодаре с широким ассортиментом продукции"
+    />
+    <Meta name="yandex-verification" content="e490c6f4b37e1903" />
+    <Meta
+      name="google-site-verification"
+      content="FNh4WtLjLHTiTomFdGINF_a52SB5CrTD0Uo-8xbUWV0"
+    />
+  </Head>
+  <NuxtPage />
+  <WidjetAudio2
+    v-if="audioWidjet"
+    :item="audioWidjet"
+    @closeWidjet="audioWidjet = null"
+  />
+</template>
