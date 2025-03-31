3425de0b62f066d08be71cf4002e38bfc3c1c4cd
Arsen Asanov <tatarin08082002@gmail.com>
2025-03-31 22:57:35 +0300

    added page principles

--- pages/principles.vue
+++ pages/principles.vue
@@ -1,4 +1,21 @@
-<script setup></script>
+<script setup>
+import CallbackBlock from '~/components/CallBackBlock.vue';
+import Clients from '~/components/Clients.vue';
+import FrontPrinciples from '~/components/FrontPrinciples.vue';
+import Sidebar from '~/components/Sidebar.vue';
+
+
+</script>
 <template>
-    <div></div>
+  <div class="container-big">
+    <div class="section__main">
+      <Sidebar class="sidebar" />
+      <div>
+        <FrontPrinciples/>
+        <Clients/>
+        <CallbackBlock/>
+      </div>
+    </div>
+  </div>
 </template>
+
